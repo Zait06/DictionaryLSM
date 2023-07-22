e924d3e568fc9126c31238410ab5b360474e6dd7
Gloria-2001 <olimen141@gmail.com>
2023-07-21 22:42:41 -0600

    Navigation bar update

--- frontend/src/layouts/default/AppBar.vue
+++ frontend/src/layouts/default/AppBar.vue
@@ -1,13 +1,57 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
+  <v-layout id="nav-container">
+    <v-bottom-navigation
+      v-model="value"
+      :elevation="0"
+      grow
+      :bg-color="color"
+      color="#D9D9D9"
+      id="navbar"
+    >
+      <v-btn>
+        <router-link to="/" class="link-bar-nav">Inicio</router-link>
+      </v-btn>
 
-      Essentials Preset
-    </v-app-bar-title>
-  </v-app-bar>
+      <v-btn>
+        <router-link to="/" class="link-bar-nav">Palabras</router-link>
+      </v-btn>
+
+      <v-btn>
+        <router-link to="/" class="link-bar-nav">Contacto</router-link>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-layout>
 </template>
 
-<script lang="ts" setup>
-  //
+<script lang="ts">
+  export default {
+    data: () => ({ value: 1 }),
+
+    computed: {
+      color () {
+        return '#3F004F'
+      },
+    },
+  }
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+.link-bar-nav{
+  color: white;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  text-decoration: none;
+  font-family: 'Roboto', sans-serif;
+}
+
+.v-bottom-navigation__content{
+  justify-content: left !important;
+}
+
+#navbar{
+  top: 0;
+}
+</style>
